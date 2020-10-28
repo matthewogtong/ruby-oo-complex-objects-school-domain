0bda8670288bbe8ca755c357dbc84a70e72cfbeb
matthewogtong <ogtongm@gmail.com>
2020-10-27 20:59:14 -0400

    modified school file

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
 # code here!
+class School
+    
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student_name, grade)
+        if @roster[grade]
+          @roster[grade] << student_name
+        else
+          @roster[grade] = []
+          @roster[grade] << student_name
+        end
+    end
+
+    def grade(grade)
+        self.roster[grade]
+    end
+
+    def sort
+        self.roster.each do |grade, students|
+            students.sort!
+        end
+        
+    end
+
+end
